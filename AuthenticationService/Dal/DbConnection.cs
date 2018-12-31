e29071de100dfcd2d5667585725d35de43f7d4e1
itamar daisy <itamardaisy@gmail.com>
2018-12-31 15:52:55 +0200

    Change the brunch name

---
+++ AuthenticationService/Dal/DbConnection.cs
@@ -0,0 +1,17 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class DbConnection
+    {
+        public void AddUserToDatabase(AuthenticationUser user)
+        {
+
+        }
+    }
+}
