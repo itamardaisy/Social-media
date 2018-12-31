7ceeadc16f93e78e79fc2b245c33e844cdf957d2
sanadsa <sanad_s96@hotmail.com>
2018-12-31 14:25:04 +0200

    First Identity Commit

---
+++ IdentityService/Common/Identity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Identity
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string WorkAddress { get; set; }
+
+        public Identity(string name, int age, string address, string workAddress)
+        {
+            FullName = name;
+            Age = age;
+            Address = address;
+            WorkAddress = workAddress;
+        }
+    }
+}
