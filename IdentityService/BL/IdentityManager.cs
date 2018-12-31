f9ab1c26caf85a54131b499ab6b887b5a9dc496f
sanadsa <sanad_s96@hotmail.com>
2018-12-31 21:01:55 +0200

    bl and dal

---
+++ IdentityService/BL/IdentityManager.cs
@@ -0,0 +1,17 @@
+﻿using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class IdentityManager : IIdentityManager
+    {
+        public void AddUser(string name, int age, string address, string workAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
