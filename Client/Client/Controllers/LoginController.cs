60119ef48558004518a2288b0341d61e4d67af94
itamar daisy <itamardaisy@gmail.com>
2019-01-06 10:14:29 +0200

    Create login controller

---
+++ Client/Client/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public ActionResult Login(string email, string password)
+        {
+
+        }
+    }
+}
